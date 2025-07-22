c573e0c4e759ef943a4cf13eec61c150a63e22c3
wshain <2286428139@qq.com>
2025-07-22 20:36:03 +0800

    handle-malloc

--- handle-malloc/handle-malloc.cu
+++ handle-malloc/handle-malloc.cu
@@ -20,7 +20,7 @@ void cpu(int *a, int *b, int *c_cpu)
         }
 }
 
-bool check(int *a, int *b, int *c_cpu, int *c_gpu)
+bool check(int *c_cpu, int *c_gpu)
 {
     for (int r = 0; r < N; r++)
         for (int c = 0; c < N; c++)
@@ -32,11 +32,13 @@ bool check(int *a, int *b, int *c_cpu, int *c_gpu)
 }
 int main()
 {
-    int *a, *b, *c_cpu, *c_gpu, *c_gpu_cpu;
+    int *a_cpu, *b_cpu, *a_gpu, *b_gpu, *c_cpu, *c_gpu, *c_gpu_cpu;
     size_t size = N * N * sizeof(int);
 
-    cudaMallocHost(&a, size);
-    cudaMallocHost(&b, size);
+    cudaMallocHost(&a_cpu, size);
+    cudaMallocHost(&b_cpu, size);
+    cudaMalloc(&a_gpu, size);
+    cudaMalloc(&b_gpu, size);
     cudaMallocHost(&c_cpu, size);
     cudaMallocHost(&c_gpu_cpu, size);
     cudaMalloc(&c_gpu, size);
@@ -44,26 +46,32 @@ int main()
     for (int r = 0; r < N; r++)
         for (int c = 0; c < N; c++)
         {
-            a[r * N + c] = r;
-            b[r * N + c] = c;
+            a_cpu[r * N + c] = r;
+            b_cpu[r * N + c] = c;
             c_cpu[r * N + c] = 0;
             // c_gpu[r * N + c] = 0;
             c_gpu_cpu[r * N + c] = 0;
         }
 
-    cpu(a, b, c_cpu);
+    cpu(a_cpu, b_cpu, c_cpu);
 
     dim3 threads(16, 16, 1);
     dim3 blocks((N + threads.x - 1) / threads.x, (N + threads.y - 1) / threads.y, 1);
-    gpu<<<blocks, threads>>>(a, b, c_gpu);
+
+    cudaMemcpy(a_gpu, a_cpu, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(b_gpu, b_cpu, size, cudaMemcpyDeviceToHost);
+
+    gpu<<<blocks, threads>>>(a_gpu, b_gpu, c_gpu);
     cudaDeviceSynchronize();
 
     cudaMemcpy(c_gpu_cpu, c_gpu, size, cudaMemcpyDeviceToHost); // 把cudamalloc() 申请的数据从c_gpu 拷贝到c_gpu_cpu, 因为cudamalloc申请的内存只能gpu使用，cpu无法访问指针
 
-    check(a, b, c_cpu, c_gpu_cpu) ? printf("ok") : printf("error");
+    check(c_cpu, c_gpu_cpu) ? printf("ok") : printf("error");
 
-    cudaFreeHost(a);
-    cudaFreeHost(b);
+    cudaFreeHost(a_cpu);
+    cudaFreeHost(b_cpu);
+    cudaFree(a_gpu);
+    cudaFree(b_gpu);
     cudaFreeHost(c_cpu);
     cudaFreeHost(c_gpu_cpu);
     cudaFree(c_gpu);
--- handle-malloc/readme.md
+++ handle-malloc/readme.md
@@ -2,6 +2,8 @@
 
 在 CUDA 编程中，GPU 可以直接使用 cudaMallocHost 分配的锁页内存，是因为这些内存页已经被固定在物理内存中，并且可以直接映射到 GPU 的地址空间中。因此，在 Kernel 函数中可以直接访问这些内存页，而不需要进行额外的数据传输或者拷贝操作。
 
-而 cudaMalloc 申请的内存仅可 gpu 访问，cpu 访问只能通过 cudaMemcpy 拷贝到 cudaMallocHost 申请的内存上之后进行访问</h5>
+而 cudaMalloc 申请的内存仅可 gpu 访问，cpu 访问只能通过 cudaMemcpy 拷贝到 cudaMallocHost 申请的内存上之后进行访问
+
+不过为了兼容，最好对于 cudaMallocHost 还是使用 cudaMemcpy 进行拷贝后使用</h5>
 <img src = "../static/手动内存管理.png" ></img>
 <img src = "../static/手动分配内存接口.png" ></img>
